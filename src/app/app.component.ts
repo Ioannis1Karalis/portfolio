bbc617bf8c0d44fe4484804502e8a9603616a35b
Ioannis Karalis <i.karalis@outlook.de>
2026-03-02 21:18:50 +0100

    skills component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { AboutMeSectionComponent } from './about-me-section/about-me-section.component';
+import { SkillsSectionComponent } from './skills-section/skills-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroSectionComponent, AboutMeSectionComponent],
+  imports: [CommonModule, RouterOutlet, HeroSectionComponent, AboutMeSectionComponent, SkillsSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
