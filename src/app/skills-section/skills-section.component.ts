8dd5ebc891ba4977db8de0a95ebf4ef1f8f068b4
Ioannis Karalis <i.karalis@outlook.de>
2026-03-21 02:50:13 +0100

    skills button enabled

--- src/app/skills-section/skills-section.component.ts
+++ src/app/skills-section/skills-section.component.ts
@@ -9,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class SkillsSectionComponent {
 
+  scrollTo(elementId: string): void {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
